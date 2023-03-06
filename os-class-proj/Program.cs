b996f54c9712229dd240a6c65303eaac047a66ab
karpenych <Dmitriy52444@gmail.com>
2023-03-06 10:14:07 +1000

    Add project files.

---
+++ os-class-proj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace os_class_proj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
